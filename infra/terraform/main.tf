e74f297325c4b155a6e51fae2778edb5b7311469
PAULO FARIA <paulorfbr@gmail.com>
2026-04-13 18:12:05 +0200

    Fix issue with use_lockfile instead of dynamo_table var

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -15,7 +15,7 @@ terraform {
     key            = "prod/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
-    dynamodb_table = "prix-strategie-tfstate-lock"
+    use_lockfile   = true
   }
 }
 
